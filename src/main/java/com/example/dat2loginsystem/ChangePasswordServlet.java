58fbb0952fd9740c13f82498e7e5ea86774b72f9
Peter537 <pean@outlook.dk>
2023-03-07 19:45:38 +0100

    added change password

---
+++ src/main/java/com/example/dat2loginsystem/ChangePasswordServlet.java
@@ -0,0 +1,29 @@
+package com.example.dat2loginsystem;
+
+import com.example.dat2loginsystem.entities.Person;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet(name = "changePasswordServlet", value = "/change-password-servlet")
+public class ChangePasswordServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Person> personMap = (Map<String, Person>) getServletContext().getAttribute("personMap");
+        Person person = (Person) request.getSession().getAttribute("person");
+        String newPassword = request.getParameter("newPassword");
+        if (newPassword.equals("")) {
+            request.setAttribute("newPasswordBesked", "Password cannot be empty");
+            request.getRequestDispatcher("WEB-INF/userSite.jsp").forward(request, response);
+        } else {
+            person.setPassword(newPassword);
+            personMap.put(person.getName(), person);
+            request.setAttribute("newPasswordBesked", "Password changed");
+            request.getRequestDispatcher("WEB-INF/userSite.jsp").forward(request, response);
+        }
+    }
+}
--- src/main/webapp/WEB-INF/userSite.jsp
+++ src/main/webapp/WEB-INF/userSite.jsp
@@ -5,13 +5,22 @@
 </head>
 <body>
 <h1>Velkommen til din side ${sessionScope.person.name}</h1>
-<p>Dit session id er: ${requestScope.id}</p>
 <p>Din kode er ${sessionScope.person.password}</p>
 
+<br>
 <form action="logout-servlet">
     <input type="submit" value="Log af">
 </form>
 
+<br>
+${requestScope.newPasswordBesked}
+<form action="change-password-servlet">
+    <label for="newPassword">Nyt Password:</label><br>
+    <input type="text" id="newPassword" name="newPassword" value=""><br><br>
+    <input type="submit" value="Skift kode">
+</form>
+
+<br>
 ${requestScope.besked}
 <form action="user-overview-servlet" method="get">
     <input type="submit" value="Gå til administrator side">
